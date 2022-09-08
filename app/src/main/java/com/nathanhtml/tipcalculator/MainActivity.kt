12d494cef53ff229aad99510b62bb366840cb73a
Nathan Flacher <nathan.flacher.pro@gmail.com>
2022-09-09 01:12:44 +0200

    create basic back of the app

--- app/src/main/java/com/nathanhtml/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/nathanhtml/tipcalculator/MainActivity.kt
@@ -2,10 +2,37 @@ package com.nathanhtml.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.nathanhtml.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("TipCalculator", "onCreate of the main activity")
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener{ calculateTip() }
     }
+
+    private fun calculateTip () {
+        Log.d("TipCalculator", "start calculate tip")
+        val stringCost = binding.editor.text.toString()
+        val cost = stringCost.toDouble()
+        val quality = binding.quality.checkedRadioButtonId
+        val tipPercentage = when (quality) {
+            R.id.amazing -> 0.20
+            R.id.good -> 0.18
+            else -> 0.15
+        }
+        var tip = cost * tipPercentage
+        val roundUp = binding.roundUpSwitch.isChecked
+        if(roundUp) tip = kotlin.math.ceil(tip)
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
 }
